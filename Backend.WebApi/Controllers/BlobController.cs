1ac2ad215c6b57d9fa7771b5779d3efb88078356
Arjan Einbu <arjan@einbu.no>
2025-02-23 00:40:00 +0000

    Download original file or preview (jpg)

--- Backend.WebApi/Controllers/BlobController.cs
+++ Backend.WebApi/Controllers/BlobController.cs
@@ -1,5 +1,8 @@
+using Backend.Core.Providers;
+using Backend.DbModel.Database;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.WebApi.Controllers;
 
@@ -9,21 +12,46 @@ namespace Backend.WebApi.Controllers;
 [Authorize]
 public class BlobController : ControllerBase
 {
-    [HttpGet]
-    public ActionResult<byte[]> GetRawFile()
+    private readonly MpaDbContext _dbContext;
+    private IFileStorageProvider _fileProvider;
+
+    public BlobController(MpaDbContext dbContext, IFileStorageProvider fileProvider)
     {
-        throw new NotImplementedException();
-        //Idea for later: Require special role or priviliges to download raw files
+        _dbContext = dbContext;
+        _fileProvider = fileProvider;
     }
 
+
     [HttpGet]
-    public ActionResult<byte[]> GetPreviewImage([FromQuery]DimensionEnum dimensions, [FromQuery]int pageNo =  1)
+    public async Task<ActionResult<FileContentResult>> GetRawFile([FromQuery] int blobId)
+    {
+        //TODO: Idea for later: Require special role or priviliges to download raw files
+
+        var blob = await _dbContext.Blobs.SingleOrDefaultAsync(blob => blob.Id == blobId);
+        if (blob == null)
+        {
+            return NotFound();
+        }
+
+        var stream = _fileProvider.GetFile(blob.PathInStore, out var metadata);
+        return File(stream, metadata.MimeType, metadata.OriginalFilename);
+    }
+
+
+    [HttpGet]
+    public async Task<ActionResult<byte[]>> Preview([FromQuery] int blobId, [FromQuery] DimensionEnum dimensions, [FromQuery] int pageNo = 1)
     {
         // IMPORTANT: GetPreviewImage must be a HTTP GET request, so that the URL can be used in <img src="..."> tags
+        // TODO: Idea for later: preview images can have watermarks
 
-        throw new NotImplementedException();
-        // Any loggged in users can download preview images for their current tenant
-        // Idea for later: preview images can have watermarks
+        var blob = await _dbContext.Blobs.SingleOrDefaultAsync(blob => blob.Id == blobId);
+        if (blob == null)
+        {
+            return NotFound();
+        }
+
+        var stream = _fileProvider.GetPreview(blob.PathInStore, 400, 400, pageNo, out var metadata);
+        return File(stream, "image/jpg", $"{metadata.OriginalFilename}_preview({pageNo}).jpg");
     }
 
 
