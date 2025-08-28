0f8c33b4036750869118808ff8f6458e4fa8c60b
Arjan Einbu <arjan@einbu.no>
2025-08-28 19:30:55 +0000

    Move and rename scripts

--- scripts/clear-and-recreate-migrations.sh
+++ scripts/clear-and-recreate-migrations.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
+# Extreme caution is advised when running this script, as it will delete existing migrations.
 rm -rf Backend.DbModel/Migrations/ && rm -rf data/Database/ && rm -rf data/Blobs && dotnet ef migrations add Initial --project=Backend.DbModel
