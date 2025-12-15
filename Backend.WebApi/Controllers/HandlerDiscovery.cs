49b4868953b421e5f0cd55c5ca8ba2a4b11ab2f1
Arjan Einbu <arjan@einbu.no>
2025-12-15 21:16:36 +0000

    Refactor: Update command handler interfaces to separate async and sync methods

--- Backend.WebApi/Controllers/HandlerDiscovery.cs
+++ Backend.WebApi/Controllers/HandlerDiscovery.cs
@@ -67,8 +67,29 @@ public interface ICommand<TCommand> where TCommand : ICommand<TCommand>
 {
 }
 
-public interface ICommandHandler<TCommand> : IHandler where TCommand : ICommand<TCommand>
+public interface IAsyncCommandHandler<TCommand> : IHandler where TCommand : ICommand<TCommand>
 {
 	Task Handle(TCommand command);
 }
+
+public interface ICommandHandler<TCommand> : IHandler where TCommand : ICommand<TCommand>
+{
+	void Handle(TCommand command);
+}
+
+
+// // Command with result interfaces
+// public interface ICommand<TCommand, TResult> where TCommand : ICommand<TCommand, TResult>
+// {
+// }
+
+// public interface IAsyncCommandHandler<TCommand, TResult> : IHandler where TCommand : ICommand<TCommand, TResult>
+// {
+// 	Task<TResult> Handle(TCommand command);
+// }
+
+// public interface ICommandHandler<TCommand, TResult> : IHandler where TCommand : ICommand<TCommand, TResult>
+// {
+// 	TResult Handle(TCommand command);
+// }
 #endregion
