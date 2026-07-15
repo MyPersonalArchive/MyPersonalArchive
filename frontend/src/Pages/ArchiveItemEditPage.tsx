16dec18bb06c2f32105d3f803d4cb660c8cd5a2a
Arjan Einbu <arjan@einbu.no>
2026-07-15 22:39:53 +0000

    Update package references in project files

--- frontend/src/Pages/ArchiveItemEditPage.tsx
+++ frontend/src/Pages/ArchiveItemEditPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { Link, useNavigate, useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { TagsInput } from "../Components/TagsInput"
 import { useApiClient } from "../Utils/Hooks/useApiClient"
 import { PreviewList } from "../Components/PreviewList"
