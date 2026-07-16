db21f006820f8a22a2d81e1795b21d4d07ca31fc
Arjan Einbu <arjan@einbu.no>
2026-07-16 22:10:48 +0000

    Add and populate Guid column to ArchiveItem table

---
+++ Backend.Mpa.DbModel/Migrations/20260716220818_Refactor_ArchiveItemId_to_Guid_pt1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Mpa.DbModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class Refactor_ArchiveItemId_to_Guid_pt1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "Guid",
+                table: "ArchiveItem",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+			migrationBuilder.Sql(@"
+				UPDATE ArchiveItem
+				SET Guid = (
+					lower(hex(randomblob(4))) || '-' ||
+					lower(hex(randomblob(2))) || '-' ||
+					'4' || substr(lower(hex(randomblob(2))), 2) || '-' ||
+					substr('89ab', 1 + (abs(random()) % 4), 1) || substr(lower(hex(randomblob(2))), 2) || '-' ||
+					lower(hex(randomblob(6)))
+				)
+			");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Guid",
+                table: "ArchiveItem");
+        }
+    }
+}
