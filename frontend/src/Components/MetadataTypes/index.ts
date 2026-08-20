282d8c0a2bde8d2859a13738459f1880431a8ca2
Arjan Einbu <arjan@einbu.no>
2026-08-20 11:48:44 +0000

    refactor: remove unused Description and Expiry metadata components

--- frontend/src/Components/MetadataTypes/index.ts
+++ frontend/src/Components/MetadataTypes/index.ts
@@ -1,15 +1,11 @@
 import { MetadataType } from "../../Utils/Metadata/types"
-import DescriptionMetadataType from "./DescriptionMetadataType"
 import EmailMetadataType from "./EmailMetadataType"
-import ExpiryMetadataComponent from "./ExpiryMetadataType"
 import ReceiptMetadataComponent from "./ReceiptMetadataType"
 import TravelDocumentMetadataType from "./TravelDocumentMetadataType"
 
 
 export const allMetadataTypes: Array<MetadataType> = [
 	ReceiptMetadataComponent,
-	ExpiryMetadataComponent,
-	DescriptionMetadataType,
 	EmailMetadataType,
 	TravelDocumentMetadataType
 ]
