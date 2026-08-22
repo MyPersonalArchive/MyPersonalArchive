739bc909765eab3fb79a7679251cf864320fadd3
Arjan Einbu <arjan@einbu.no>
2026-08-22 08:16:43 +0000

    feat: simplify editing and creating legs in TravelDocumentMetadataType

--- frontend/src/Components/MetadataTypes/TravelDocumentMetadataType.tsx
+++ frontend/src/Components/MetadataTypes/TravelDocumentMetadataType.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react"
+import { useRef } from "react"
 import { MetadataComponentProps, MetadataType } from "../../Utils/Metadata/types"
 import { changeAtIndex, removeAtIndex } from "../../Utils/array-helpers"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -9,17 +9,19 @@ type Command =
 	| { action: "METADATA_LOADED", metadata: State }
 	| { action: "SET_NOTES", notes: string }
 	| { action: "ADD_LEG", leg: Leg }
-	| { action: "UPDATE_LEG", index: number, leg: Leg }
+	| { action: "UPDATE_LEG_BOOKINGREF", index: number, bookingRef: string }
+	| { action: "UPDATE_LEG_ROUTENUMBER", index: number, routeNumber: string }
+	| { action: "UPDATE_LEG_DEPARTUREFROM", index: number, departureFrom: string }
+	| { action: "UPDATE_LEG_ARRIVALAT", index: number, arrivalAt: string }
 	| { action: "REMOVE_LEG", index: number }
 
 type State = {
-	notes: string,
 	legs: Leg[]
 }
 
 type Leg = {
 	bookingRef: string,
-	flightNumber: string,
+	routeNumber: string,
 	departureFrom: string,
 	arrivalAt: string,
 }
@@ -28,7 +30,6 @@ const reducer = (state: State, command: Command): State => {
 	switch (command.action) {
 		case "INIT":
 			return {
-				notes: "",
 				legs: []
 			}
 
@@ -38,22 +39,34 @@ const reducer = (state: State, command: Command): State => {
 				...command.metadata
 			}
 
-		case "SET_NOTES":
-			return {
-				...state,
-				notes: command.notes
-			}
-
 		case "ADD_LEG":
 			return {
 				...state,
 				legs: [...state.legs, command.leg]
 			}
 
-		case "UPDATE_LEG":
+		case "UPDATE_LEG_BOOKINGREF":
 			return {
 				...state,
-				legs: changeAtIndex(state.legs, command.index, command.leg)
+				legs: changeAtIndex(state.legs, command.index, { ...state.legs[command.index], bookingRef: command.bookingRef })
+			}
+
+		case "UPDATE_LEG_ROUTENUMBER":
+			return {
+				...state,
+				legs: changeAtIndex(state.legs, command.index, { ...state.legs[command.index], routeNumber: command.routeNumber })
+			}
+
+		case "UPDATE_LEG_DEPARTUREFROM":
+			return {
+				...state,
+				legs: changeAtIndex(state.legs, command.index, { ...state.legs[command.index], departureFrom: command.departureFrom })
+			}
+
+		case "UPDATE_LEG_ARRIVALAT":
+			return {
+				...state,
+				legs: changeAtIndex(state.legs, command.index, { ...state.legs[command.index], arrivalAt: command.arrivalAt })
 			}
 
 		case "REMOVE_LEG":
@@ -71,7 +84,8 @@ const Component = (props: MetadataComponentProps) => {
 	const state = props.state as State
 	const dispatch = props.dispatch as React.Dispatch<Command>
 
-	const [editingRowIndex, setEditingRowIndex] = useState<number | null>(null)
+	const areThereEmptyLegs = state.legs.some(leg => !leg.bookingRef && !leg.routeNumber && !leg.departureFrom && !leg.arrivalAt)
+	const isLastLegEmpty = state.legs.length > 0 && !state.legs.at(-1)?.bookingRef && !state.legs.at(-1)?.routeNumber && !state.legs.at(-1)?.departureFrom && !state.legs.at(-1)?.arrivalAt
 
 	return (<>
 
@@ -92,28 +106,20 @@ const Component = (props: MetadataComponentProps) => {
 						leg={leg}
 						index={index}
 						dispatch={dispatch}
-						isEditing={editingRowIndex === index}
-						setIsEditing={() => setEditingRowIndex(index)}
-						endIsEditing={() => setEditingRowIndex(null)}
 					/>
 				))}
-				<NewRow
-					dispatch={dispatch}
-					isEditing={editingRowIndex === state.legs.length}
-					setIsEditing={() => setEditingRowIndex(state.legs.length)}
-					endIsEditing={() => setEditingRowIndex(null)}
-				/>
+				{!isLastLegEmpty &&
+					<tr className="h-12.75">
+						<td colSpan={6}>
+							<button type="button" className="w-full" onClick={() => dispatch({ action: "ADD_LEG", leg: { bookingRef: "", routeNumber: "", departureFrom: "", arrivalAt: "" } })}>
+								<FontAwesomeIcon icon={faPlus} />
+							</button>
+						</td>
+					</tr>
+				}
 			</tbody>
 
 		</table>
-
-		<textarea
-			rows={4}
-			value={state.notes} onChange={e => dispatch({ action: "SET_NOTES", notes: e.target.value })}
-			className="input w-full my-2"
-			placeholder="Write your notes here..."
-		>
-		</textarea>
 	</>)
 }
 
@@ -122,150 +128,31 @@ type RowProps = {
 	leg: Leg,
 	index: number,
 	dispatch: React.Dispatch<Command>,
-	isEditing: boolean,
-	setIsEditing: () => void
-	endIsEditing: () => void
 }
-const Row = ({ leg, index, dispatch, isEditing, setIsEditing, endIsEditing }: RowProps) => {
+const Row = ({ leg, index, dispatch }: RowProps) => {
 	const firstInputRef = useRef<HTMLInputElement>(null)
 
-	const [bookingRef, setBookingRef] = useState(leg.bookingRef)
-	const [flightNo, setFlightNo] = useState(leg.flightNumber)
-	const [departureFrom, setDepartureFrom] = useState(leg.departureFrom)
-	const [arrivalAt, setArrivalAt] = useState(leg.arrivalAt)
-
-	const removeLeg = () => {
-		dispatch({ action: "REMOVE_LEG", index })
-		endIsEditing()
-	}
-
-	const updateLeg = () => {
-		dispatch({
-			action: "UPDATE_LEG",
-			index: index,
-			leg: { bookingRef, flightNumber: flightNo, departureFrom, arrivalAt }
-		})
-		endIsEditing()
-	}
-
-	const cancelEditing = () => {
-		setBookingRef(leg.bookingRef)
-		setFlightNo(leg.flightNumber)
-		setDepartureFrom(leg.departureFrom)
-		setArrivalAt(leg.arrivalAt)
-
-		endIsEditing()
-	}
-
 	return (
-		isEditing
-			? <tr className="group">
-				<td>{index + 1}</td>
-				<td>
-					<input type="text" className="input w-full" value={flightNo} onChange={e => setFlightNo(e.target.value)} />
-				</td>
-				<td>
-					<input type="text" className="input w-full" value={departureFrom} onChange={e => setDepartureFrom(e.target.value)} />
-				</td>
-				<td>
-					<input type="text" className="input w-full" value={arrivalAt} onChange={e => setArrivalAt(e.target.value)} />
-				</td>
-				<td>
-					<input type="text" className="input w-full" value={bookingRef} onChange={e => setBookingRef(e.target.value)} ref={firstInputRef} />
-				</td>
-				<td className="flex justify-end gap-2">
-					<button type="button" className="btn btn-primary" onClick={() => { updateLeg() }}>
-						Save
-					</button>
-					<button type="button" className="btn" onClick={() => { cancelEditing() }}>
-						Revert
-					</button>
-					<button type="button" className="text-red-500" onClick={() => { removeLeg() }}>
-						<FontAwesomeIcon icon={faTrash} />
-					</button>
-
-				</td>
-			</tr>
-			: <tr className="group">
-				<td onClick={setIsEditing}>{index + 1}</td>
-				<td onClick={setIsEditing}>{flightNo}</td>
-				<td onClick={setIsEditing}>{departureFrom}</td>
-				<td onClick={setIsEditing}>{arrivalAt}</td>
-				<td onClick={setIsEditing}>{bookingRef}</td>
-				<td className="flex justify-end gap-2">
-					<button type="button" className="text-gray-400 group-hover:text-red-500" onClick={() => {
-						dispatch({ action: "REMOVE_LEG", index })
-					}}>
-						<FontAwesomeIcon icon={faTrash} />
-					</button>
-				</td>
-			</tr>)
-}
-
-
-type NewRowProps = {
-	dispatch: React.Dispatch<Command>,
-	isEditing: boolean,
-	setIsEditing: (index?: number) => void
-	endIsEditing: () => void
-}
-const NewRow = ({ dispatch, isEditing, setIsEditing, endIsEditing }: NewRowProps) => {
-	const [bookingRef, setBookingRef] = useState("")
-	const [flightNo, setFlightNo] = useState("")
-	const [departureFrom, setDepartureFrom] = useState("")
-	const [arrivalAt, setArrivalAt] = useState("")
-
-	const addLeg = () => {
-		dispatch({ action: "ADD_LEG", leg: { bookingRef, flightNumber: flightNo, departureFrom, arrivalAt } })
-
-		setBookingRef("")
-		setFlightNo("")
-		setDepartureFrom("")
-		setArrivalAt("")
-
-		endIsEditing()
-	}
-
-	const cancelEditing = () => {
-		setBookingRef("")
-		setFlightNo("")
-		setDepartureFrom("")
-		setArrivalAt("")
-
-		endIsEditing()
-	}
-
-	return (
-		isEditing
-			? <tr className="group">
-				<td></td>
-				<td>
-					<input type="text" className="input w-full" value={flightNo} onChange={e => setFlightNo(e.target.value)} />
-				</td>
-				<td>
-					<input type="text" className="input w-full" value={departureFrom} onChange={e => setDepartureFrom(e.target.value)} />
-				</td>
-				<td>
-					<input type="text" className="input w-full" value={arrivalAt} onChange={e => setArrivalAt(e.target.value)} />
-				</td>
-				<td>
-					<input type="text" className="input w-full" value={bookingRef} onChange={e => setBookingRef(e.target.value)} />
-				</td>
-				<td className="flex justify-end gap-2">
-					<button type="button" className="btn btn-primary" onClick={addLeg}>Save</button>
-					<button type="button" className="text-gray-400 group-hover:text-red-500" onClick={cancelEditing}>
-						<FontAwesomeIcon icon={faTrash} />
-					</button>
-				</td>
-			</tr>
-			: <tr>
-				<td colSpan={6}>
-					<button type="button" className="w-full" onClick={() => setIsEditing()}>
-						<FontAwesomeIcon icon={faPlus} />
-					</button>
-				</td>
-			</tr>
-	)
+		<tr className="group">
+			<td>{index + 1}</td>
+			<td>
+				<input type="text" className="input" value={leg.routeNumber} onChange={e => dispatch({ action: "UPDATE_LEG_ROUTENUMBER", index, routeNumber: e.target.value })} />
+			</td>
+			<td>
+				<input type="text" className="input" value={leg.departureFrom} onChange={e => dispatch({ action: "UPDATE_LEG_DEPARTUREFROM", index, departureFrom: e.target.value })} />
+			</td>
+			<td>
+				<input type="text" className="input" value={leg.arrivalAt} onChange={e => dispatch({ action: "UPDATE_LEG_ARRIVALAT", index, arrivalAt: e.target.value })} />
+			</td>
+			<td>
+				<input type="text" className="input" value={leg.bookingRef} onChange={e => dispatch({ action: "UPDATE_LEG_BOOKINGREF", index, bookingRef: e.target.value })} ref={firstInputRef} />
+			</td>
+			<td>
+				<button type="button" className="text-gray-400 group-hover:text-red-500" onClick={() => { dispatch({ action: "REMOVE_LEG", index }) }}>
+					<FontAwesomeIcon icon={faTrash} />
+				</button>
+			</td>
+		</tr>)
 }
 
 
