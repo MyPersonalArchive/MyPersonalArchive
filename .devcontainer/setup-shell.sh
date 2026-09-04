33df776a6a3d4ef48c27ec874b64bcee46367bd7
Arjan Einbu <arjan@einbu.no>
2026-09-04 20:17:46 +0000

    switch from npm to pnpm

--- .devcontainer/setup-shell.sh
+++ .devcontainer/setup-shell.sh
@@ -7,7 +7,13 @@ echo "source /usr/share/bash-completion/completions/git" >> ~/.bashrc
 echo "source .devcontainer/bash/dotnet-autocompletion" >> ~/.bashrc
 
 # bash parameter completion for npm
-echo "source <(npm completion)" >> ~/.bashrc
+# echo "source <(npm completion)" >> ~/.bashrc
+
+# bash parameter completion for pnpm
+echo "source <(pnpm completion bash)" >> ~/.bashrc
+
+# bash parameter completion for docker
+echo "source <(docker completion bash)" >> ~/.bashrc
 
 #
 dotnet tool restore
