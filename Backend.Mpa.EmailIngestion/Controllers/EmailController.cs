fabb752831bba7c3fa1a043e17ad1b1a2e24b240
Arjan Einbu <arjan@einbu.no>
2026-09-26 21:36:16 +0000

    refactor: stop streaming emails when connection is aborted or broken

--- Backend.Mpa.EmailIngestion/Controllers/EmailController.cs
+++ Backend.Mpa.EmailIngestion/Controllers/EmailController.cs
@@ -5,26 +5,9 @@ using Microsoft.AspNetCore.Http;
 
 namespace Backend.Mpa.EmailIngestion.Controllers;
 
-//TODO:
-/*
-	- We should know which attachments that has been added to our system so that we dont search/show them again (provider + unique messageId?)
-		- We should be able to filter away unique email ids during search
-	- We should be able to tell our system which emails/attachments we are not interested in to filter them out for future searches
-		- Same as above, exclude provider + unique messageId?
-	- We need to find a secure way to store oauth2 tokens and basic imap passwords
-	- We should include the complete email when user adds an attachments
-	- Implement more providers (maybe just outlook/hotmail for now? Rest can come later)
-		- Outlook/hotmail (is this the same now?)
-		- Zoho
-		- Apple
-		- Yahoo
-	- UI for fetching and adding attachments/emails
-*/
-
 
 [ApiController]
 [Route("api/[controller]")]
-// [Authorize(Policy = "TenantIdPolicy")]
 public class EmailController : ControllerBase
 {
 	private readonly ImapClientFactory _imapClientFactory;
@@ -62,12 +45,16 @@ public class EmailController : ControllerBase
 		var imapClient = await _imapClientFactory.GetImapClient(externalAccountId);
 
 		Response.Headers.ContentType = "text/event-stream";
-		
+
 		await foreach (var email in imapClient.GetEmailsStreaming(folder))
 		{
+			if (HttpContext.RequestAborted.IsCancellationRequested)
+				break;
+
 			var json = System.Text.Json.JsonSerializer.Serialize(email, JsonSerializerDefaults.Options);
 			await Response.WriteAsync($"data: {json}\n\n");
 			await Response.Body.FlushAsync();
 		}
+
 	}
 }
